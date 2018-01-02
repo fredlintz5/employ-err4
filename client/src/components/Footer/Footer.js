cfb5192fe0193f1305e0189687b8680a0dd8f217
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2018-01-01 20:42:57 -0700

    cleaned up footer text on login, and removed unnecessary functions in pageUser

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import "./Footer.css";
 
 
-function Footer() {
+function Footer(props) {
 	return (
 		<footer className="row">
 			<h2 className="swipetext text-center">SWIPE NOW</h2>
-			<h3 className="text-white">Login and start recruiting!</h3>
+			<h3 className="text-white" style={props.hideText ? {display: "none"} : {display: "block"}}>Login and start recruiting!</h3>
 		</footer>
 	)
 }
