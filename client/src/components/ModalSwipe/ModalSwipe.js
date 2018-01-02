9d610dab70e319fa6a5a9f3e2b708a299f422b48
Fred Lintz <fredlintz@Freds-MacBook-Pro.local>
2018-01-01 17:46:35 -0700

    sprucing up modal

--- client/src/components/ModalSwipe/ModalSwipe.js
+++ client/src/components/ModalSwipe/ModalSwipe.js
@@ -17,8 +17,8 @@ function ModalSwipe(props) {
 					<div className="modal-body">
 						<div id="modal-image">
 							<img src={props.data[0] ? props.data[0].image : "https://tinyurl.com/ybgx3yx8"} alt="Employer Modal"/>
-							<a onClick={() => props.thumbsDown()}><i className="fa fa-thumbs-down" aria-hidden="false"></i></a>
-							<a onClick={() => props.thumbsUp()}><i className="fa fa-thumbs-up" aria-hidden="false"></i></a>
+							<a style={props.data[0] ? {display: "block"} : {display: 'none'}} onClick={() => props.thumbsDown()}><i className="fa fa-thumbs-down" aria-hidden="false"></i></a>
+							<a style={props.data[0] ? {display: "block"} : {display: 'none'}} onClick={() => props.thumbsUp()}><i className="fa fa-thumbs-up" aria-hidden="false"></i></a>
 						</div>
 						<div id="modal-text">
 							<p>{props.data[0] ? props.data[0].title.slice(0,35) : "YOU ARE OUT OF MATCHES"}</p>
